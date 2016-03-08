d62868fe0f8a5520abfdee40f39319bbaf4e20ad
Hendrik Brummermann <nhnb@users.sourceforge.net>
2016-03-08 23:44:58 +0100

    reader data from CVS

--- loginfo.py
+++ loginfo.py
@@ -9,41 +9,52 @@
 
 import sys
 import os
-from argparse import ArgumentParser
-from argparse import RawDescriptionHelpFormatter
+import getopt
+import datetime
 
 def escape(data):
     return data.replace("\\", "\\\\").replace("\"", "\\\"")
 
 class CvsReader:
-    def __init__(self, args):
+    def __init__(self):
         self.meta = {}
         self.commits = []
-        self.args = args
+        self.commits.append({})
+        self.urls = []
+        self.args = []
 
     def read_commandline_arguments(self):
         """parse command line arguments"""
-        # self.args["default_email_domain"]
-        parser = ArgumentParser(description="webhook for CVS commits", formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("--default-email-domain", dest="default_email_domain", action="store", default="example.com", help="email domain to append to account names without @ [default: %(default)s]")
-        parser.add_argument("--url", dest="url", action="append", required=True, help="webhook url [default: %(default)s]")
-        parser.add_argument("--repository", dest="repository", required=True, help="name of repository")
-        parser.add_argument("--home-url", dest="repo_url", help="URL to ViewVC")
-        parser.add_argument("--commitid", dest="commitid", required=True, help="commitid CVS variable %%I", metavar="%I")
-        parser.add_argument("--folder", dest="folder", required=True, help="folder CVS variable %%p", metavar="%p")
-        parser.add_argument(dest="cvsargs", help="file information CVS variable %%{sVv}", metavar="cvsargs", nargs='+')
-        args = parser.parse_args()
-        
-        print(args.default_email_domain)
-        print(args.url)
-        print(args.repository)
-        print(args.repo_url)
-        print(args.cvsargs)
+
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "", ["default-email-domain=", "url=", "repository=", "home-url=", "commitid=", "folder="])
+        except getopt.GetoptError as err:
+            # print help information and exit:
+            print str(err) # will print something like "option -a not recognized"
+            sys.exit(2)
+
+        self.meta["default_email_domain"] = "example.com"
+
+        for o, a in opts:
+            if o == "--default-email-domain":
+                self.meta["default_email_domain"] = a
+            elif o == "--url":
+                self.urls.append(a)
+            elif o == "--repository":
+                self.meta["repository"] = a
+            elif o == "--home-url":
+                self.meta["home_url"] = a
+            elif o == "--commitid":
+                self.meta["commitid"] = a
+            elif o == "--folder":
+                self.meta["folder"] = a
+        self.args = args
+
 
     def read_stdin(self):
         """read branch and commit message from stdin"""
 
-        self.meta["branch"] = ""
+        self.meta["branch"] = "HEAD"
         message = ""
         wait_for_start_of_message = True
         for line in sys.stdin:
@@ -65,40 +76,58 @@ class CvsReader:
         else:
             full_name = os.environ["USER"]
 
-        self.username = full_name
+        self.meta["username"] = full_name
 
         if full_name.find("@") < 0: 
             full_name = full_name.replace("#", "@")
 
         if full_name.find("@") < 0:
-            full_name = full_name + "@" + self.args["default_email_domain"]
+            full_name = full_name + "@" + self.meta["default_email_domain"]
 
-        self.email = full_name
-        self.real_name = full_name[0:full_name.find("@")]
+        self.meta["email"] = full_name
+        self.meta["real_name"] = full_name[0:full_name.find("@")]
+
+    def build_file_lists(self):
+        commit = self.commits[0]
+        commit["added"] = []
+        commit["removed"] = []
+        commit["modified"] = []
+        commit["revisions"] = {}
+
+        while len(self.args) > 0:
+            filename = self.meta["folder"] + "/" + self.args.pop(0)
+            old = self.args.pop(0)
+            new = self.args.pop(0)
+            if old == new and new == "NONE":
+                continue
+            if old == "NONE":
+                commit["added"].append(filename)
+                commit["revisions"][filename] = new
+            elif new == "NONE":
+                commit["removed"].append(filename)
+                commit["revisions"][filename] = self.meta["commitid"] # todo
+            else:
+                commit["modified"].append(filename)
+                commit["revisions"][filename] = new
+
+    def build_commit(self):
+        self.commits[0]["commitid"] = self.meta["commitid"]
+        self.commits[0]["message"] = self.meta["message"]
+        self.commits[0]["timestamp"] = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
 
     def read(self):
         self.read_commandline_arguments()
         self.read_stdin()
         self.read_author()
-        ### TODO: implement me ###
-
-        #self.meta["commitid"]
-        #self.meta["module"]
-        #self.meta["cvs_repo"]
-        #self.meta["html_url"]
-        
-        #commit["message"]
-        #commit["timestamp"]
-        # changetype in ("added", "removed", "modified"):
-        #revision_map
-
+        self.build_file_lists()
+        self.build_commit()
 
 class OutputGenerator:
     def __init__(self, meta, commits):
         self.meta = meta
         self.commits = commits
-        self.ouptput = ""
+        self.output = ""
 
     def write(self):
         self.write_header()
@@ -108,14 +137,14 @@ class OutputGenerator:
 
     def write_header(self):
         self.output += "{\n"
-        self.output += "\"ref\": \"refs/heads/" + escape(self.meta["tag"]) + "\"},\n"
-        self.output += "\"after\": \"" + escape(self.meta["commitid"]) + "\"},\n"
+        self.output += "\"ref\": \"refs/heads/" + escape(self.meta["branch"]) + "\",\n"
+        self.output += "\"after\": \"" + escape(self.meta["commitid"]) + "\",\n"
 
     def write_user(self):
         self.output += "{\n"
-        self.output += "\"name\": \"" + escape(self.meta["real_name"]) + "\"},\n"
-        self.output += "\"email\": \"" + escape(self.meta["email"]) + "\"},\n"
-        self.output += "\"username\": \"" + escape(self.meta["username"]) + "\"}\n"
+        self.output += "\"name\": \"" + escape(self.meta["real_name"]) + "\",\n"
+        self.output += "\"email\": \"" + escape(self.meta["email"]) + "\",\n"
+        self.output += "\"username\": \"" + escape(self.meta["username"]) + "\"\n"
         self.output += "},\n"
 
     def write_file_list(self, files):
@@ -131,22 +160,28 @@ class OutputGenerator:
 
     def write_revision_map(self, commit):
         self.output += "{"
-        for filename, revision in commit.revisions:
+        for filename, revision in commit["revisions"].items():
             self.output += "\"" + escape(filename) + "\": \"" + escape(revision) + "\",\n"
         self.output += "},"
 
     def write_commit(self, commit):
         self.output += "{\n"
-        self.output += "\"id\": \"" + escape(commit["commitid"]) + "\"},\n"
-        self.output += "\"distinct\": \"true\"},\n"
-        self.output += "\"message\": \"" + escape(commit["message"]) + "\"},\n"
-        self.output += "\"timestamp\": \"" + escape(commit["timestamp"]) + "\"},\n" ## TODO "2016-03-06T13:12:42+02:00"
-        self.output += "\"author\": " + self.write_user()
-        self.output += "\"committer\": " + self.write_user()
+        self.output += "\"id\": \"" + escape(commit["commitid"]) + "\",\n"
+        self.output += "\"distinct\": \"true\",\n"
+        self.output += "\"message\": \"" + escape(commit["message"]) + "\",\n"
+        self.output += "\"timestamp\": \"" + escape(commit["timestamp"]) + "\",\n"
+        self.output += "\"author\": "
+        self.write_user()
+        self.output += "\"committer\": "
+        self.write_user()
 
         for changetype in ("added", "removed", "modified"):
-            self.output += "\"" + changetype + "\": \"" + self.write_file_list(commit["files"][changetype])
-        self.output += "\"revisions\": " + self.write_revision_map(commit)
+            self.output += "\"" + changetype + "\": "
+            self.write_file_list(commit[changetype])
+            self.output += "\n"
+        self.output += "\"revisions\": "
+        self.write_revision_map(commit)
+        self.output += "\n"
         self.output += "},\n"
 
     def write_commits(self):
@@ -159,10 +194,10 @@ class OutputGenerator:
 
     def write_repository(self):
         self.output += "\"repository\": {\n"
-        self.output += "\"name\": \"" + escape(self.meta["module"]) + "\",\n"
-        self.output += "\"full_name\": \"" + escape(self.meta["cvs_repo"] + "/" + self.meta["module"]) + "\",\n"
-        self.output += "\"html_url\": \"" + escape(self.meta["html_url"]) + "\",\n"
-        self.output += "\"url\": \"" + escape(self.meta["html_url"]) + "\"\n"
+        self.output += "\"name\": \"" + escape(self.meta["repository"]) + "\",\n"
+        self.output += "\"full_name\": \"" + escape(self.meta["repository"]) + "\",\n"
+        self.output += "\"home_url\": \"" + escape(self.meta["home_url"]) + "\",\n"
+        self.output += "\"url\": \"" + escape(self.meta["home_url"]) + "\"\n"
         self.output += "}\n"
 
     def write_footer(self):
@@ -172,16 +207,12 @@ class OutputGenerator:
 def main(argv=None):
     '''Command line options.'''
 
-    ### TODO: implement command line argument parser
-
-    with open("/tmp/out", "a") as out:
-        out.write("------------------------\n")
-        out.write(" ".join(sys.argv)+ "\n")
-        for line in sys.stdin:
-            out.write(line)
-        out.write("---\n")
-        for key in os.environ.keys():
-            out.write("%30s %s \n" % (key,os.environ[key]))
+    reader = CvsReader()
+    reader.read()
+    
+    output = OutputGenerator(reader.meta, reader.commits)
+    output.write()
+    print(output.output)
 
 if __name__ == "__main__":
     sys.exit(main())
